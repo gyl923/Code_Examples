4f36f420fbb5c0292f21d7eb563e976fd80dd1e3
gyl923 <bajsy923@naver.com>
2021-10-28 22:06:23 +0900

    Create 3__3_4.py

---
+++ greedy/3__3_4.py
@@ -0,0 +1,11 @@
+# 숫자 카드 게임
+
+# 행마다 min을 뽑아 비교
+# 그중 max인것을 찾아 선택
+
+n,m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)] # m개씩 입력
+cnt=0
+for i in range(n):
+    cnt = max(cnt, min(arr[i]))
+print(cnt)
