f099172fc3f5e3e68e61bfda24b3ce61ef858d4c
gyl923 <bajsy923@naver.com>
2021-11-09 17:37:27 +0900

    Create 5_11.py

---
+++ DFS_BFS/5_11.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+g = []
+for i in range(n):
+    g.append(list(map(int, input().rstrip())))
+# print(g)
+def bfs(a,b):
+    queue = deque()
+    queue.append([a,b])
+    dir = [[1,0],[-1,0],[0,-1],[0,1]]
+    while queue:
+        a,b = queue.popleft()
+        for i in dir:
+            A = a+i[0]
+            B = b+i[1]
+            if A<0 or A>n-1 or B<0 or B>m-1:
+                continue
+            if g[A][B]==1:
+                queue.append([A,B])
+                g[A][B] = g[a][b]+1
+    return g[n-1][m-1]
+print(bfs(0,0))
