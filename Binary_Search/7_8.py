1a9b7947d45374c5a3e0d4cc9f95135a5b79991c
gyl923 <bajsy923@naver.com>
2021-12-30 00:58:06 +0900

    Edit 7_8.py

--- Binary_Search/7_8.py
+++ Binary_Search/7_8.py
@@ -3,3 +3,25 @@
 n개의 떡이 주어진다. n개의 떡 모두 일정길이h 만큼 자르고 남은 떡의 길이 합이 
 m보다 크거나 같도록 만드는 h를 구하는 문제
 '''
+# 1
+n, m = map(int, input().split())
+arr = sorted(map(int, input().split()))
+
+def bs(arr,start,end,target):
+    ans = 0
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for i in arr:
+            if i > mid:
+                cnt += i-mid
+        # if cnt == target:
+        #     return mid
+        if cnt < target:
+            end = mid - 1
+        else:
+            ans = mid
+            start = mid + 1
+    return ans
+ans = bs(arr,0,max(arr),6)
+print(ans)
