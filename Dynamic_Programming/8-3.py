294889c525f8341eef54bc25c3ba55a9c6fce9a3
gyl923 <bajsy923@naver.com>
2022-01-01 13:45:28 +0900

    Create 8-3.py

---
+++ Dynamic_Programming/8-3.py
@@ -0,0 +1,15 @@
+# 다이나믹프로그래밍 예시 피보나치수열
+# 재귀함수로 구현 + 메모이제이션
+# 호출되는 함수 확인
+
+d = [0] * 100
+
+def fibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+fibo(6)
